4304c613ef12c93a0e068c139f7a42eafb51b329
Pratik Gagare <pratikgagare302002@gmail.com>
2023-07-28 14:22:30 +0530

    project upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function displaysearch(){
+    document.getElementById('search').style.display = "inline";
+}
+
+function displaynav(){
+    document.getElementById('mobilenav').style.display = "inline";
+}
+
+function closenav(){
+    document.getElementById('mobilenav').style.display = "none";
+}
+
+
+
+
